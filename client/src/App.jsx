59a83a1cfbad3f27be11488ba2894d7e02a015ae
Thu Ra Lynn Htun <lynnhtunthura8@gmail.com>
2025-04-14 19:56:14 +0630

    Setting up

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Home from "./pages/Home";
+import BuyCredit from "./pages/BuyCredit";
+import Result from "./pages/Result";
+
+const App = () => {
+  return (
+    <div>
+      <Home />
+      <Result />
+      <BuyCredit />
+    </div>
+  );
+};
+
+export default App;
---
+++ client/src/pages/BuyCredit.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const BuyCredit = () => {
+  return <div>Buy redit</div>;
+};
+
+export default BuyCredit;
