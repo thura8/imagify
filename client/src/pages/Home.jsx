6243228b5df24137ed22409ca0e2678de0490f49
Thu Ra Lynn Htun <lynnhtunthura8@gmail.com>
2025-04-15 11:50:41 +0630

    Add list to header

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "../components/Header";
+import List from "../components/List";
 
 const Home = () => {
   return (
     <div>
       <Header />
+      <List />
     </div>
   );
 };
