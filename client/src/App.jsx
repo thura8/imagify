03266f9afe8f7626f3705acd7fff348794b54fb7
Thu Ra Lynn Htun <lynnhtunthura8@gmail.com>
2025-04-15 15:52:09 +0630

    Add Footer component to all pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,7 @@ import BuyCredit from "./pages/BuyCredit";
 import Result from "./pages/Result";
 import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
@@ -14,6 +15,8 @@ const App = () => {
         <Route path="/result" element={<Result />} />
         <Route path="/buy" element={<BuyCredit />} />
       </Routes>
+
+      <Footer />
     </div>
   );
 };
