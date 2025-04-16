51710e8e0f74abd980edce741efc6f0f13e06f14
Thu Ra Lynn Htun <lynnhtunthura8@gmail.com>
2025-04-16 17:51:43 +0630

    Finish login ui and add context to login to be visible

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import { Link, useNavigate } from "react-router-dom";
 import { AppContext } from "../context/AppContext";
 
 const Navbar = () => {
-  const { user, setUser } = useContext(AppContext);
+  const { user, setShowLogin } = useContext(AppContext);
   const navigate = useNavigate();
   return (
     <div className="flex items-center justify-between py-4">
@@ -43,7 +43,10 @@ const Navbar = () => {
             <p onClick={() => navigate("/buy")} className="cursor-pointer">
               Pricing
             </p>
-            <button className="bg-zinc-800 text-white px-7 py-2 sm:px-10 text-sm rounded-full">
+            <button
+              className="bg-zinc-800 text-white px-7 py-2 sm:px-10 text-sm rounded-full"
+              onClick={() => setShowLogin(true)}
+            >
               Login
             </button>
           </div>
--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -4,9 +4,13 @@ export const AppContext = createContext();
 
 const AppContextProvider = (props) => {
   const [user, setUser] = useState(null);
+  const [showLogin, setShowLogin] = useState(false);
+
   const value = {
     user,
     setUser,
+    showLogin,
+    setShowLogin,
   };
 
   return (
