aff4efda236f684e3bf2a2764680cdcbce9c94a4
Thu Ra Lynn Htun <lynnhtunthura8@gmail.com>
2025-04-16 18:35:25 +0630

    Add framer motion to components in header

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -17,7 +17,7 @@ const Login = () => {
 
   return (
     <div
-      className="absolute top-0 left-0 right-0 bottom-0 z-10
+      className="fixed top-0 left-0 right-0 bottom-0 z-10
    backdrop-blur-sm bg-black/30 flex justify-center items-center"
     >
       <form className="relative bg-white p-10 rounded-xl text-slate-500">
