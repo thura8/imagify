51828e7e2c5cc00062cb46ff01493f238ede0819
Thu Ra Lynn Htun <lynnhtunthura8@gmail.com>
2025-04-18 16:41:56 +0630

    Implement image generation function using API

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,6 +1,7 @@
 import { createContext, useEffect, useState } from "react";
 import { toast } from "react-toastify";
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 export const AppContext = createContext();
 
@@ -12,6 +13,8 @@ const AppContextProvider = (props) => {
   const [credit, setCredit] = useState(false);
 
   const backendUrl = import.meta.env.VITE_BACKEND_URL;
+
+  const navigate = useNavigate();
 
   const loadCreditsData = async () => {
     try {
@@ -25,6 +28,29 @@ const AppContextProvider = (props) => {
       }
     } catch (error) {
       console.log(error);
+      toast.error(error.message);
+    }
+  };
+
+  const generateImage = async (prompt) => {
+    try {
+      const { data } = await axios.post(
+        backendUrl + "/api/image/generate-image",
+        { prompt },
+        { headers: { token } }
+      );
+
+      if (data.success) {
+        loadCreditsData();
+        return data.resultImage;
+      } else {
+        toast.error(data.message);
+        loadCreditsData();
+        if (data.creditBalance <= 0) {
+          navigate("/buy");
+        }
+      }
+    } catch (error) {
       toast.error(error.message);
     }
   };
@@ -53,6 +79,7 @@ const AppContextProvider = (props) => {
     setCredit,
     loadCreditsData,
     logout,
+    generateImage,
   };
 
   return (
--- client/src/pages/Result.jsx
+++ client/src/pages/Result.jsx
@@ -1,6 +1,7 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import { assets } from "../assets/assets";
 import { motion } from "framer-motion";
+import { AppContext } from "../context/AppContext";
 
 const Result = () => {
   const [image, setImage] = useState(assets.sample_img_1);
@@ -8,7 +9,21 @@ const Result = () => {
   const [loading, setLoading] = useState(false);
   const [input, setInput] = useState("");
 
-  const onSubmitHandler = async (e) => {};
+  const { generateImage } = useContext(AppContext);
+
+  const onSubmitHandler = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+
+    if (input) {
+      const image = await generateImage(input);
+      if (image) {
+        setIsImageLoaded(true);
+        setImage(image);
+      }
+    }
+    setLoading(false);
+  };
 
   return (
     <motion.form
